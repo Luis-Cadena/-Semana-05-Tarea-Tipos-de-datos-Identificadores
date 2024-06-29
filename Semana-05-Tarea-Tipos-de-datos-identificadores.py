00666fffb3ff5402f635240d444e5043760f921d
Luis-Cadena <159209664+Luis-Cadena@users.noreply.github.com>
2024-06-29 09:32:18 -0500

    Add files via upload
    actualización

--- Semana-05-Tarea-Tipos-de-datos-identificadores.py
+++ Semana-05-Tarea-Tipos-de-datos-identificadores.py
@@ -4,26 +4,14 @@
 
 import math
 
+def calcular_area_circulo(radio):#Calcula el área de un círculo dado su radio.
 
-def calcular_area_circulo(radio):
-    """
-    Calcula el área de un círculo dado su radio.
-
-    Parámetros:
-    radio (float): El radio del círculo.
-
-    Devuelve:
-    float: El área del círculo.
-    """
     area = math.pi * radio ** 2
     return area
 
 
 def main():
-    """
-    Función principal del programa. Solicita al usuario el radio del círculo,
-    calcula el área y muestra el resultado.
-    """
+
     # Solicita al usuario que ingrese el radio del círculo
     radio_str = input("Introduce el radio del círculo: ")
 
